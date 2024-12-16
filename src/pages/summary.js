da0b2ad0de6fd200123048a001d6515e5435caa9
SpaceShade <120098872+SpaceShade@users.noreply.github.com>
2024-12-16 16:41:50 +0700

    edit increase-decrease

--- src/pages/summary.js
+++ src/pages/summary.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "../components/navbar";
-// import remove from '../assets/remove.png'
+
 const Summary = () => {
   const [foodItems, setFoodItems] = useState([]);
 
@@ -8,29 +8,52 @@ const Summary = () => {
     const savedItems = JSON.parse(localStorage.getItem("foodItems")) || [];
     setFoodItems(savedItems);
   }, []);
+  
+  const handleIncrement = (index) => {
+    const updatedItems = [...foodItems];
+    updatedItems[index].amount += 1;
+    setFoodItems(updatedItems);
+    localStorage.setItem("foodItems",JSON.stringify(updatedItems));
+  }
+  const handleDecrement = (index) => {
+    const updatedItems = [...foodItems];
+    if (updatedItems[index].amount > 1){
+      updatedItems[index].amount -= 1; 
+    } else{
+      updatedItems.splice(index, 1);
+    }
+    setFoodItems(updatedItems);
+    localStorage.setItem("foodItems",JSON.stringify(updatedItems));
+  }
 
-  // const handleRemoveClick()=>{
-    
-  // }
   return (
     <div>
       <Navbar />
       <div className="p-4">
         {foodItems.length > 0 ? (
           foodItems.map((item, index) => (
+            <>
             <div key={index} className="mt-2 flex justify-between">
               <p className="text-xl">{item.name}</p>
               <div className="flex items-center ">
-                <p className="text-xl">x{item.amount}</p>
-                {/* <img src={remove} alt="remove" className="w-4 h-4 ml-2"/> */}
+               <button onClick={()=>handleDecrement(index)} className="text-2xl mx-4">
+                -
+              </button> 
+              <p className="text-xl">{item.amount}</p>
+              <button onClick={()=>handleIncrement(index)} className="text-2xl mx-4">
+                +
+              </button>
               </div>
             </div>
+            </>
           ))
         ) : (
           <p className="text-red-500 mt-4">No order details available.</p>
         )}
-        <button className="mt-4">Confirm</button>
       </div>
+      <div className="justify-center flex">
+              <button className=" absolute bottom-4 text-xl border-black border-2 px-2 m-2 rounded-md">Confirm</button>
+            </div>
     </div>
   );
 };
