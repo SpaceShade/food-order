ab2b3eaf479ab417d06e7e1d711058949812683c
SpaceShade <120098872+SpaceShade@users.noreply.github.com>
2024-12-17 15:06:07 +0700

    show amount of food

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import Navbar from '../components/navbar';
 const Home = () => 
     {
-        const [food, setfood] = useState([
+        const [food, setFood] = useState([
         {id:1 ,name: 'หมูสามชั้นสไลด์', img:'https://www.mcfs.co.th/wp-content/uploads/2022/07/shabu-product.jpg' },
         {id:2 ,name: 'เบคอนรมควัน', img:'https://www.mcfs.co.th/wp-content/uploads/2022/07/bacon-product.jpg' },
         {id:3 ,name: 'พอร์คชอปติดสามชั้น',img:'https://www.mcfs.co.th/wp-content/uploads/2022/07/porkchop-product.jpg' },
@@ -15,8 +15,19 @@ const Home = () =>
     const navigate = useNavigate();
     const handleFoodClick =(foodItem)=>{
         navigate(`/detail`,{ state: foodItem });
-    }
-
+      }
+      useEffect (()=>{
+        const savedItems = JSON.parse(localStorage.getItem('foodItems')) || [];
+        const updatedFood = food.map ((item) => {
+          const cartItem = savedItems.find((cart)=> cart.name === item.name);
+          if (cartItem){
+            return {...item, amount: cartItem.amount };
+          }
+          return item;
+        })
+          setFood(updatedFood);
+      },[])
+    
   return (
     <div>
         <Navbar/>
@@ -25,8 +36,14 @@ const Home = () =>
             <div key={foodItem.id} 
             onClick={()=> handleFoodClick(foodItem)}
             className='mb-2 p-4 shadow-md w-5/6 rounded-md '>
+              <img src={foodItem.img} alt='food-img' className='mb-2'/>
+              <div className='flex justify-between'>
               <h1>{foodItem.name}</h1>     
+              <h2>{foodItem.amount > 0 ? `x${foodItem.amount}` : 'x0'}</h2>
+
+              </div>
             </div> 
+            
             ))}
         
       </div>
